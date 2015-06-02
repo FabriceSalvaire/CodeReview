5e956b89ebe5f7c6678c06b00354d78e222f9ec5
Fabrice Salvaire <fabrice.salvaire@orange.fr>
2015-06-02 22:10:21 +0200

    disabled italic and bold highlight

--- CodeReview/GUI/DiffViewer/SyntaxHighlighterStyle.py
+++ CodeReview/GUI/DiffViewer/SyntaxHighlighterStyle.py
@@ -65,10 +65,11 @@ class SyntaxHighlighterStyle(dict):
                 text_format.setBackground(to_brush(style_attributes['bgcolor']))
             if style_attributes['color']:
                 text_format.setForeground(to_brush(style_attributes['color']))
-            if style_attributes['bold']:
-                text_format.setFontWeight(QtGui.QFont.Bold)
-            if style_attributes['italic']:
-                text_format.setFontItalic(True)
+            # Fixme: font is rendered smaller ???
+            # if style_attributes['bold']:
+            #     text_format.setFontWeight(QtGui.QFont.Bold)
+            # if style_attributes['italic']:
+            #     text_format.setFontItalic(True)
             self[token] = text_format
 
     ##############################################
