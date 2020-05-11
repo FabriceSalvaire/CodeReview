388272f9c3521b215125a86879f2af4f444db0d1
Fabrice Salvaire <fabrice.salvaire@orange.fr>
2020-05-11 14:11:05 +0200

    setenv

--- setenv.sh
+++ setenv.sh
@@ -1,6 +1,6 @@
 source /opt/python-virtual-env/py36/bin/activate
 
 # append_to_ld_library_path_if_not /usr/local/lib
-append_to_ld_library_path_if_not /usr/local/stow/libgit2-26/lib
+append_to_ld_library_path_if_not /usr/local/stow/libgit2-27/lib
 
 append_to_python_path_if_not ${PWD}
