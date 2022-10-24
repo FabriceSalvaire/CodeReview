6298ba76e0de7d5fda229ae25c292ffdf2cf5257
Fabrice SALVAIRE <fabrice.salvaire@orange.fr>
2022-10-24 21:54:21 +0200

    cleanup

--- CodeReview/GUI/Widgets/IconLoader.py
+++ CodeReview/GUI/Widgets/IconLoader.py
@@ -26,7 +26,7 @@ from PyQt5 import QtGui
 ####################################################################################################
 
 from CodeReview.Common.Singleton import SingletonMetaClass
-import CodeReview.Config.ConfigInstall as ConfigInstall
+from CodeReview.Config import ConfigInstall
 
 ####################################################################################################
 
@@ -37,53 +37,44 @@ class IconLoader(object, metaclass=SingletonMetaClass):
     ##############################################
 
     def __init__(self):
-
         self._cache = {}
 
     ##############################################
 
     def _mangle_icon_name(self, icon_name, icon_size):
-
         return icon_name + '@' + str(icon_size)
 
     ##############################################
 
     def _demangle_icon_name(self, icon_name):
-
         if '@' in icon_name:
             icon_name, icon_size = icon_name.split('@')
             if icon_size != 'svg':
                 icon_size = int(icon_size)
         else:
             icon_size = self.icon_size
-
         return icon_name, icon_size
 
     ##############################################
 
     def __getitem__(self, icon_name):
-
         icon_name, icon_size = self._demangle_icon_name(icon_name)
         return self.get_icon(icon_name, icon_size)
 
     ##############################################
 
     def get_icon(self, icon_name, icon_size=icon_size):
-
         mangled_icon_name = self._mangle_icon_name(icon_name, icon_size)
         if mangled_icon_name not in self._cache:
             icon_path = self._find(icon_name, icon_size)
             self._cache[mangled_icon_name] = QtGui.QIcon(icon_path)
-
         return self._cache[mangled_icon_name]
 
     ##############################################
 
     def _find(self, file_name, icon_size):
-
         if icon_size == 'svg':
             extension = '.svg'
         else:
             extension = '.png'
-
         return ConfigInstall.Icon.find(file_name + extension, icon_size)
