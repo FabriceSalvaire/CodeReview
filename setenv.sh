f836379591411367e911a11f296485ab244ece69
Fabrice SALVAIRE <fabrice.salvaire@orange.fr>
2022-10-24 20:53:25 +0200

    setenv

--- setenv.sh
+++ setenv.sh
@@ -1,6 +1,6 @@
-source /opt/python-virtual-env/py38/bin/activate
+source /opt/python-virtual-env/py310/bin/activate
 
 # append_to_ld_library_path_if_not /usr/local/lib
-append_to_ld_library_path_if_not /usr/local/stow/libgit2-27/lib
+# append_to_ld_library_path_if_not /usr/local/stow/libgit2-27/lib
 
 append_to_python_path_if_not ${PWD}
